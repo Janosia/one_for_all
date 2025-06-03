68f844cdd5ad942316d81f62014d415b80a1b3ed
Janosia <janosia.banerjee03@gmail.com>
2025-06-03 22:27:40 +0530

    only map for easy navigation

--- one_for_all/events.py
+++ one_for_all/events.py
@@ -7,18 +7,11 @@ from checks.media import lightdm, auto_play_devices
 event_map = {
     "on_user_login": [cleardesk, sshlogindis, sshrootlogin],
     "on_network_up": [ipspoofing, sourcepackedrouting, directedpingsignored, synattacksblocked],
-    "on_system_boot": [ipv6dis, ipspoofing, sourcepackedrouting, synattacksblocked, sshlogindis, sshrootlogin],
+    # "on_system_boot": [ipv6dis, ipspoofing, sourcepackedrouting, synattacksblocked, sshlogindis, sshrootlogin],
     "on_usb_device_inserted": [auto_play_devices],
     "on_firefox_start": [read_addons],
     "on_lightdm_loaded": [lightdm],
     "on_sysctl_change": [ipv6dis, ipspoofing, sourcepackedrouting, synattacksblocked, directedpingsignored]
 }
-def trigger_event(event_name):
-    checks = event_map.get(event_name)
-    if not checks:
-        print(f" No checks associated with event: {event_name}")
-        return
-    print(f" Running checks for event: {event_name}")
-    for check in checks:
-        check()
-        print("-" * 40)
+
+
